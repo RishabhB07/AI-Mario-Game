f5a2c10f4bcdc311be78207406ac63c8e299eee5
RishabhB07 <73791841+RishabhB07@users.noreply.github.com>
2021-11-12 17:18:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,13 @@
-function preload() {
+function preload(){
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
 }
-
-function setup() {
+function setup(){
 	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
 	instializeInSetup(mario);
 }
-
-function draw() {
+function draw(){
 	game()
 }
-
-
-
-
-
-
